fd16ac679bb981bcded7a17f889eeab1a3651da6
Nakib Hossin <hussain0296@gmail.com>
2020-01-14 22:06:39 +0600

    Scrapping Full Page

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ scrapy genspider <file_name> <domain_name>
 ### To run scrapy class run command 
 ```
 scrapy runspider <file_name>
-scrapy runspider <file_name> -o <file_name_to_write>
+scrapy runspider <file_name> -o <file_where_to_WRITE_scrapped_data>
 ```
---
+++ Scrapping/full_page.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class FullPageSpider(scrapy.Spider):
+    name = 'full_page'
+    allowed_domains = ['toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        cells_list = response.css('div.quote')
+        for cell in cells_list:
+            yield{
+                'author_name': cell.css('small.author::text').extract_first(),
+                'text':cell.css('span.text::text').extract_first(),
+                'tags':cell.css('a.tag::text').extract()
+            }
